cf77042485cc6858f8163415e2f6751a0de1370d
ArunKumarM <arunmnayaka@gmail.com>
2025-02-23 08:36:27 -0600

    Merged color code changes

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
             <input
               type="text"
               id="search"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full pl-10 p-2.5"
               placeholder="Search Doctors"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)} // Update state
@@ -64,7 +64,7 @@ export default function Home() {
           </div>
           <button
             type="submit"
-            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-pink-700 rounded-lg border border-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300"
           >
             <svg className="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
